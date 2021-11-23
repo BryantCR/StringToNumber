83bcb708486372a2f40edfc1e2f307c03896143d
BryantCR <bryancvargas20@gmail.com>
2021-11-23 09:57:05 -0600

    ok

---
+++ StringToNumber.js
@@ -0,0 +1,13 @@
+function stringToNUmber(word){
+    if(word != null){
+        if(word != ""){
+            
+        }
+        else{
+            console.log("Error data content");
+        }
+    }
+    else{
+        console.log("Error data content");
+    }
+}
